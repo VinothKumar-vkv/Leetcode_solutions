9040473879f99fb265e989dbe4724bd41d250c5d
Vinoth Kumar V <vinothkumarvkv.in@gmail.com>
2026-07-11 18:48:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
+
         stack<char> st;
-        for (char ch : s) {
-            // Push opening brackets
-            if (ch == '(' || ch == '[' || ch == '{') {
-                st.push(ch);
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
             }
-            // For closing brackets
             else {
-                if (st.empty()) return false;  // No matching opening
-                char top = st.top();
-                if ((ch == ')' && top != '(') ||
-                    (ch == ']' && top != '[') ||
-                    (ch == '}' && top != '{')) {
+
+                if(st.empty())
+                    return false;
+
+                char ch = st.top();
+
+                if((s[i] == ')' && ch == '(') ||
+                   (s[i] == '}' && ch == '{') ||
+                   (s[i] == ']' && ch == '[')) {
+
+                    st.pop();
+                }
+                else {
                     return false;
                 }
-                st.pop();
             }
         }
-        return st.empty();  // All brackets must be closed
+
+        return st.empty();
     }
 };
